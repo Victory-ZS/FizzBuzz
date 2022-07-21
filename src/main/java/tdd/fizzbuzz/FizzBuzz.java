964206681d6bc282d11653e1ea1643a35fd1c281
VictoryZhao <victory.zhao@oocl.com>
2022-07-21 11:51:57 +0800

    feat: say Fizz if order 3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,6 +4,9 @@ public class FizzBuzz {
     public static String FIZZ = "Fizz";
 
     public String countOff(int order){
+        if(order % 3 == 0){
+            return "Fizz";
+        }
         return String.valueOf(order);
 
     }
