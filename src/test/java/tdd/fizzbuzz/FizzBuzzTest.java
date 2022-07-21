dfdee222b96f4fe23249f018ee0501e5e4610af8
VictoryZhao <victory.zhao@oocl.com>
2022-07-21 11:43:49 +0800

    test:say 1 if order 1

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,21 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_say_order_number_when_countOff_given_order_is_1(){
+        //given
+
+        FizzBuzz fizzBizz = new FizzBuzz();
+
+        //when
+
+        String actual = fizzBizz.countOff(1);
+
+        //then
+        assertEquals("1", actual);
+    }
 }
