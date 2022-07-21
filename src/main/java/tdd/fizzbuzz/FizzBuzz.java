ede236794b967b6f701761a7647fb8b9cb214d01
VictoryZhao <victory.zhao@oocl.com>
2022-07-21 13:41:55 +0800

    feat: say FizzBuzz if order 15

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,9 +3,13 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
     public static String FIZZ = "Fizz";
     public static String BUZZ = "Buzz";
+    public static String FIZZBUZZ = "FizzBuzz";
 
     public String countOff(int order){
         if(order % 3 == 0){
+            if(order % 5 == 0){
+                return FIZZBUZZ;
+            }
             return FIZZ;
         }
         else if(order % 5 == 0){
