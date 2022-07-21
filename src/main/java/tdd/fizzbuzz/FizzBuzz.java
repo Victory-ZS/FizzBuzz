4c48859952a550af14600b63c4ffbbfd25e3a1a7
VictoryZhao <victory.zhao@oocl.com>
2022-07-21 11:58:43 +0800

    feat: say Buzz if order 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,10 +2,14 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public static String FIZZ = "Fizz";
+    public static String BUZZ = "Buzz";
 
     public String countOff(int order){
         if(order % 3 == 0){
             return FIZZ;
+        }
+        else if(order % 5 == 0){
+            return BUZZ;
         }
         return String.valueOf(order);
     }
